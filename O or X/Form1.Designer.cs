e2c38d798a48ea9039863dccb6146e0232038188
Agnieszka Szmidtke <agnes.swan7@gmail.com>
2020-04-15 12:52:45 +0200

    gra

--- O or X/Form1.Designer.cs
+++ O or X/Form1.Designer.cs
@@ -28,13 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn23 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn33 = new System.Windows.Forms.Button();
+            this.btn32 = new System.Windows.Forms.Button();
+            this.btn31 = new System.Windows.Forms.Button();
+            this.komunikat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn11.Location = new System.Drawing.Point(12, 12);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(110, 106);
+            this.btn11.TabIndex = 0;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn12.Location = new System.Drawing.Point(128, 12);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(110, 106);
+            this.btn12.TabIndex = 1;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn13.Location = new System.Drawing.Point(244, 12);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(110, 106);
+            this.btn13.TabIndex = 2;
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn23
+            // 
+            this.btn23.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn23.Location = new System.Drawing.Point(244, 124);
+            this.btn23.Name = "btn23";
+            this.btn23.Size = new System.Drawing.Size(110, 106);
+            this.btn23.TabIndex = 5;
+            this.btn23.UseVisualStyleBackColor = true;
+            this.btn23.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn22.Location = new System.Drawing.Point(128, 124);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(110, 106);
+            this.btn22.TabIndex = 4;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn21.Location = new System.Drawing.Point(12, 124);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(110, 106);
+            this.btn21.TabIndex = 3;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn33
+            // 
+            this.btn33.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn33.Location = new System.Drawing.Point(244, 236);
+            this.btn33.Name = "btn33";
+            this.btn33.Size = new System.Drawing.Size(110, 106);
+            this.btn33.TabIndex = 8;
+            this.btn33.UseVisualStyleBackColor = true;
+            this.btn33.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn32
+            // 
+            this.btn32.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn32.Location = new System.Drawing.Point(128, 236);
+            this.btn32.Name = "btn32";
+            this.btn32.Size = new System.Drawing.Size(110, 106);
+            this.btn32.TabIndex = 7;
+            this.btn32.UseVisualStyleBackColor = true;
+            this.btn32.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn31
+            // 
+            this.btn31.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn31.Location = new System.Drawing.Point(12, 236);
+            this.btn31.Name = "btn31";
+            this.btn31.Size = new System.Drawing.Size(110, 106);
+            this.btn31.TabIndex = 6;
+            this.btn31.UseVisualStyleBackColor = true;
+            this.btn31.Click += new System.EventHandler(this.button_Click);
+            // 
+            // komunikat
+            // 
+            this.komunikat.AutoSize = true;
+            this.komunikat.Location = new System.Drawing.Point(70, 361);
+            this.komunikat.Name = "komunikat";
+            this.komunikat.Size = new System.Drawing.Size(0, 13);
+            this.komunikat.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(368, 400);
+            this.Controls.Add(this.komunikat);
+            this.Controls.Add(this.btn33);
+            this.Controls.Add(this.btn32);
+            this.Controls.Add(this.btn31);
+            this.Controls.Add(this.btn23);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "X or O ?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn23;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn33;
+        private System.Windows.Forms.Button btn32;
+        private System.Windows.Forms.Button btn31;
+        private System.Windows.Forms.Label komunikat;
     }
 }
 
--- O or X/Form1.cs
+++ O or X/Form1.cs
@@ -12,9 +12,70 @@ namespace O_or_X
 {
     public partial class Form1 : Form
     {
+        bool ruch = false;
         public Form1()
         {
             InitializeComponent();
         }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button przycisk = (Button)sender;
+            if (ruch)
+            {
+                przycisk.Text = "X";
+                przycisk.ForeColor = Color.Maroon;
+                komunikat.Text = "Kolej O";
+                komunikat.ForeColor = Color.Green;
+            }
+            else
+            {
+                przycisk.Text = "O";
+                przycisk.ForeColor = Color.Green;
+                komunikat.Text = "Kolej X";
+                komunikat.ForeColor = Color.Maroon;
+            }
+            ruch = !ruch;
+
+            Wynik();
+        }
+        private void Wynik()
+        {
+            bool find_winner = false;
+            //poziom
+            if ((btn11.Text == btn12.Text) && (btn12.Text == btn13.Text) && btn11.Text != "")
+                find_winner = true;
+            else if ((btn21.Text == btn22.Text) && (btn22.Text == btn23.Text) && btn21.Text != "")
+                find_winner = true;
+            else if ((btn31.Text == btn32.Text) && (btn32.Text == btn33.Text) && btn31.Text != "")
+                find_winner = true;
+            //pion
+            else if ((btn11.Text == btn21.Text) && (btn21.Text == btn31.Text) && btn21.Text != "")
+                find_winner = true;
+            else if ((btn12.Text == btn22.Text) && (btn22.Text == btn32.Text) && btn22.Text != "")
+                find_winner = true;
+            else if ((btn13.Text == btn23.Text) && (btn23.Text == btn33.Text) && btn23.Text != "")
+                find_winner = true;
+            //skos
+            else if ((btn11.Text == btn22.Text) && (btn22.Text == btn33.Text) && btn11.Text != "")
+                find_winner = true;
+            else if ((btn13.Text == btn22.Text) && (btn22.Text == btn31.Text) && btn22.Text != "")
+                find_winner = true;
+
+            if (find_winner)
+            {
+                string winner = "";
+                if (ruch)
+
+                    winner = "O";
+                else
+                    winner = "X"; 
+                komunikat.Text = "Zwycięzcą jest: " + winner;
+                komunikat.ForeColor = Color.Red;
+               
+            }
+        }
+
+       
     }
+
 }
